febfb205ea35102353cab2e830f7a6b2902e28a0
AronDas <arondas.sg@gmail.com>
2026-04-08 20:00:44 -0300

    Add files via upload

---
+++ tuple-homework.py
@@ -0,0 +1,10 @@
+import math
+
+tup1 = (4, 3, 2, 2, -1, 18)
+tup2= (2, 4, 8, 8, 3, 2, 9)
+
+tup1ans = math.prod(tup1)
+tup2ans = math.prod(tup2)
+
+print("All the numbers in", tup1, "multiplied together is:", tup1ans)
+print("All the numbers in", tup2, "multiplied together is:", tup2ans)
