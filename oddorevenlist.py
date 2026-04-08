6d86d88bdb4ac2040cd23a14717489f99401d656
AronDas <arondas.sg@gmail.com>
2026-04-08 19:14:45 -0300

    Add files via upload

---
+++ oddorevenlist.py
@@ -0,0 +1,11 @@
+user_input = input("Please put in a number here:")
+user_input2 = input("Please put in a second number here thats greater than the first one:")
+
+
+list = []
+print()
+
+list = [user_input, user_input2]
+print("the numbers between", list, "are:")
+anotherlist = (list[user_input][user_input2])
+print(anotherlist)
