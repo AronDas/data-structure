26884d1dd2eb89d92a3463296142104867b902fa
AronDas <arondas.sg@gmail.com>
2026-04-08 19:36:06 -0300

    Add files via upload

---
+++ tupple.py
@@ -0,0 +1,21 @@
+tuple = ("tuple", False, 3.2, 1)
+print(tuple)
+
+tuple = (4, 6, 8, 3, 1)
+print(tuple)
+
+
+tuple = tuple + (9,)
+
+tuple1 = (50, 10, 60, 70, 50)
+print(tuple1.count(50))
+
+
+tuple = (2, 4, 3, 5, 6, 7, 8, 6, 1)
+
+slice = tuple[3:5]
+
+print(slice)
+
+slice = tuple[:6]
+print(slice)
