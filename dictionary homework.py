7b6ee02aafced465fe87df06fe2f1071469e3f7a
AronDas <arondas.sg@gmail.com>
2026-04-21 20:04:46 -0300

    Add files via upload

---
+++ dictionary homework.py
@@ -0,0 +1,12 @@
+my_dict = {
+    "id_1" : {"name" : "aron"},
+    "id_2" : {"name" : "arthur"},
+    "id_3" : {"name" : "Max"},
+}
+i = "a"
+a = 0
+for i in my_dict:
+    a = a+1
+
+print("The leter a mentioned in the names aron, arthur, and max are", a)
+
