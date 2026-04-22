892370cc1d32587809225b3d74c2c0b2df520557
AronDas <arondas.sg@gmail.com>
2026-04-22 19:24:27 -0300

    Add files via upload

---
+++ intersections.py
@@ -0,0 +1,9 @@
+set1 = {"yellow", "purple"}
+set2 = {"green", "purple"}
+print("Original sets", set1)
+print(set2)
+
+print("Sets after intersection:")
+set3 = set1.intersection(set2)
+print(set3)
+
