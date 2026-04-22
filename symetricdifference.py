f635326fcdb0bfea38be2875259b3f095d242b1e
AronDas <arondas.sg@gmail.com>
2026-04-22 19:51:22 -0300

    Add files via upload

---
+++ symetricdifference.py
@@ -0,0 +1,12 @@
+set1 = {'blue', 'green'}
+set2 = {'blue', 'purple'}
+
+print("Original sets:")
+print(set1)
+print(set2)
+
+set3 = set1.symmetric_difference(set2)
+
+
+print("After symmetric difference:")
+print(set3)
